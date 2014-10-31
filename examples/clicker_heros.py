2ec0efcb293c379973f862d77280e7821c1c382f
顏孜羲 <joseph.yen@gmail.com>
2014-11-01 02:20:31 +0800

    clicker_heros example

--- examples/clicker_heros.py
+++ examples/clicker_heros.py
@@ -1,79 +1,84 @@
 
-#
-# CURRENTLY UNUSABLE
-#
+from __future__ import division
+from clickmachine import Click, Clicks, Actions, Sleep, Repeat, Move, Coord, CoordsMap
 
-from mouse_click import MouseMove, go_click
-import time
+def clicker_heros(left, top, right, bottom):
 
-def main():
-    mouse_moves = [ 1,
-                    MouseMove('next level', (1098, 187), 1, 0.5),
-                    MouseMove('next level', (1098, 187), 1, 0.5),
-                    MouseMove('attack boss', (1022, 622), 850, 0.5),
-                    MouseMove('pre level', (947, 187), 1, 0.5),
-                    # MouseMove('pre level', (947, 187), 1, 2),
-                    [
-                        5,
-                        MouseMove('attack', (1022, 622), 800, 0),
-                        [
-                            1,
-                            MouseMove('upgrade 1, skill 1', (297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 2', ((538-297)*1/6+297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 3', ((538-297)*2/6+297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 4', ((538-297)*3/6+297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 5', ((538-297)*4/6+297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 6', ((538-297)*5/6+297, 349), 1, 0),
-                            MouseMove('upgrade 1, skill 7', (538, 349), 1, 0),
+    coords_map = CoordsMap({
+        'next area': Coord(0.8032264379698926, 0.06998040328931704),
+        'prev area': Coord(0.6912833969301786, 0.0686068020732221),
+        'candy positions': [
+            Coord(x=0.4526320850949831, y=0.7132470101280196),
+            Coord(x=0.6466923343762987, y=0.6264048887993504),
+            Coord(x=0.6598055344710929, y=0.5448068716690171),
+            Coord(x=0.7566330192988663, y=0.7538567669700799),
+            Coord(x=0.8736523593799909, y=0.6595117305543855),
+            Coord(x=0.9141425416527853, y=0.6438832011623719)
+        ],
+        'attack': Coord(0.7516082388539517, 0.7132103807622571),
+        'scroll up': Coord(0.48161988205675976, 0.2959347508897883),
+        'scroll down': Coord(0.4817023118111534, 0.9686086335415103),
+        'heros skills': Coord(0.17087001171189428, 0.2893414650525326).mxn_points_to(
+            Coord(0.36077672439277586, 0.9823266629949208), 7, 5), 
+        'heros upgrade/hire': Coord(0.08318879504871941, 0.2879922834136127).n_points_to(
+            Coord(0.08301363182063286, 0.9558066702075053), 5),
+    }).project_to_space( left = 39.0, top = 198.9609375,
+                         right = 1174.453125, bottom = 838.98046875)
 
-                            MouseMove('upgrade 2, skill 1', (297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 2', ((538-297)*1/6+297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 3', ((538-297)*2/6+297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 4', ((538-297)*3/6+297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 5', ((538-297)*4/6+297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 6', ((538-297)*5/6+297, 467), 1, 0),
-                            MouseMove('upgrade 2, skill 7', (538, 467), 1, 0),
+    
+    enable_hero_skills = Actions([ coord.to_click() for coord in coords_map['heros skills']])
+    upgrade_heros = Actions([ coord.to_click() for coord in coords_map['heros upgrade/hire']])
+    pick_candies = Actions([ coord.to_click() for coord in coords_map['candy positions']])
+    
+    scroll_up = coords_map['scroll up'].to_click()
+    scroll_to_bottom = coords_map['scroll down'].to_clicks(50, 0.01)
+    go_next_area = coords_map['next area'].to_click()
+    go_prev_area = coords_map['prev area'].to_click()
 
-                            MouseMove('upgrade 3, skill 1', (297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 2', ((538-297)*1/6+297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 3', ((538-297)*2/6+297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 4', ((538-297)*3/6+297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 5', ((538-297)*4/6+297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 6', ((538-297)*5/6+297, 582), 1, 0),
-                            MouseMove('upgrade 3, skill 7', (538, 582), 1, 0),
-                    
-                            MouseMove('upgrade 4, skill 1', (297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 2', ((538-297)*1/6+297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 3', ((538-297)*2/6+297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 4', ((538-297)*3/6+297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 5', ((538-297)*4/6+297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 6', ((538-297)*5/6+297, 702), 1, 0),
-                            MouseMove('upgrade 4, skill 7', (538, 702), 1, 0),
+    attack = Repeat(Actions([
+        coords_map['attack'].to_clicks(800),
+        pick_candies
+    ]), times = 3)
 
-                            MouseMove('upgrade 5, skill 1', (297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 2', ((538-297)*1/6+297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 3', ((538-297)*2/6+297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 4', ((538-297)*3/6+297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 5', ((538-297)*4/6+297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 6', ((538-297)*5/6+297, 819), 1, 0),
-                            MouseMove('upgrade 5, skill 7', (538, 819), 1, 0),
+    boss_attack = Actions([
+        go_next_area,
+        Sleep(1),
+        go_next_area, go_next_area,
+        coords_map['attack'].to_clicks(850),
+        Sleep(1),
+        go_prev_area,
+        Sleep(1),
+        go_next_area,
+        Sleep(1),
+        go_prev_area,
+        Sleep(1),
+    ])
 
-                            [
-                                5,
-                                MouseMove('upgrade 5', (173, 819), 1, 0),
-                                MouseMove('upgrade 4', (173, 702), 1, 0),
-                                MouseMove('upgrade 3', (173, 582), 1, 0),
-                                MouseMove('upgrade 2', (173, 467), 1, 0),
-                                MouseMove('upgrade 1', (173, 349), 1, 0),
-                            ],
-                        ],
-                        MouseMove('upgrade 1, skill 1 sleep', (297, 349), 1, 5),
+    do_upgrades = Actions([
+        scroll_to_bottom,
+        scroll_up, 
+        Repeat(Actions([
+            scroll_up,
+            enable_hero_skills,
+            upgrade_heros,
+        ]), times = 5)
+    ])
 
-                    ],
-                ]
-    while True:
-        time.sleep(3)
-        go_click(mouse_moves)
+    whole_procesure = Repeat(Actions([
+        boss_attack,
+        Repeat(Actions([
+            attack,
+            do_upgrades
+        ]), times = 5 )
+    ]))
+
+    whole_procesure.act()
+    
+    
         
 if __name__ == '__main__':
-    main()
+    raw_input ('Move the mouse point to TOP-LEFT CORNER of Clicker Heros and press <ENTER>.')
+    left, top = Coord.current_coord()
+    raw_input ('Move the mouse point to RIGHT-BOTTOM CORNER of Clicker Heros and press <ENTER>.')
+    right, bottom = Coord.current_coord()
+    clicker_heros(left, top, right, bottom)
